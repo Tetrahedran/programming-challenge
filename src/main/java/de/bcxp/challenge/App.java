255b6d33742c84ae3e9c2966fba2d8199374c56c
Patrick <paddy38@web.de>
2022-11-13 00:57:12 +0100

    added factory for weatherData objects

--- src/main/java/de/bcxp/challenge/App.java
+++ src/main/java/de/bcxp/challenge/App.java
@@ -1,5 +1,18 @@
 package de.bcxp.challenge;
 
+import de.bcxp.challenge.data.AttributeListProvider;
+import de.bcxp.challenge.data.CSVFileReader;
+import de.bcxp.challenge.data.DataProvider;
+import de.bcxp.challenge.data.WeatherDataFactory;
+import de.bcxp.challenge.logic.BasicMetricEvaluator;
+import de.bcxp.challenge.model.WeatherData;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -13,8 +26,18 @@ public final class App {
     public static void main(String... args) {
 
         // Your preparation code …
+        List<WeatherData> data = new ArrayList<>();
+        String filePath = "C:\\Users\\Patrick Hanselmann\\Documents\\Arbeit\\programming-challenge\\src\\main\\resources\\de\\bcxp\\challenge\\weather.csv";
+        try(CSVFileReader provider = new CSVFileReader(filePath, ',')){
+            DataProvider<WeatherData> dataProvider = new WeatherDataFactory();
+            data = dataProvider.getDataObjectsFrom(provider);
+        }
+        catch(Exception fe){
+            System.out.println(fe.getMessage());
+        }
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        String dayWithSmallestTempSpread = BasicMetricEvaluator.getIdentifierForMinimumValue(
+          data, wData -> Integer.toString(wData.getDay()), WeatherData::getTemperatureSpread);     // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread: %s%n", dayWithSmallestTempSpread);
 
         String countryWithHighestPopulationDensity = "Some country"; // Your population density analysis function call …
---
+++ src/main/java/de/bcxp/challenge/data/WeatherDataFactory.java
@@ -0,0 +1,32 @@
+package de.bcxp.challenge.data;
+
+import de.bcxp.challenge.model.WeatherData;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class WeatherDataFactory implements DataProvider<WeatherData>{
+  public static final String MAXIMUM_TEMP_IDENTIFIER = "MxT";
+  public static final String MINIMUM_TEMP_IDENTIFIER = "MnT";
+  public static final String DAY_IDENTIFIER = "Day";
+
+
+  @Override
+  public List<WeatherData> getDataObjectsFrom(AttributeListProvider provider) {
+
+    List<WeatherData> data = new ArrayList<>();
+
+    while(provider.hasNewAttributeList()){
+      Map<String, String> attributes = provider.getNextAttributeList();
+      int day = Integer.parseInt(attributes.get(DAY_IDENTIFIER));
+      int maxTemp = Integer.parseInt(attributes.get(MAXIMUM_TEMP_IDENTIFIER));
+      int minTemp = Integer.parseInt(attributes.get(MINIMUM_TEMP_IDENTIFIER));
+
+      WeatherData wData = new WeatherData(day, minTemp, maxTemp);
+      data.add(wData);
+    }
+
+    return data;
+  }
+}
