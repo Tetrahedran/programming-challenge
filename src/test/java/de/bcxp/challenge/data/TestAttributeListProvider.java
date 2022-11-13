f9e2b195635701f5b0cea43e15054b447cba8adb
Patrick <paddy38@web.de>
2022-11-13 19:50:23 +0100

    added mock AttributeListProvider for testing

---
+++ src/test/java/de/bcxp/challenge/data/TestAttributeListProvider.java
@@ -0,0 +1,32 @@
+package de.bcxp.challenge.data;
+
+
+import java.util.List;
+import java.util.Map;
+
+public class TestAttributeListProvider implements AttributeListProvider{
+  private final List<Map<String, String>> data;
+  private int i;
+
+  public TestAttributeListProvider(List<Map<String, String>> data){
+    this.data = data;
+    i = 0;
+  }
+
+  @Override
+  public Map<String, String> getNextAttributeList() {
+    if(hasNewAttributeList()){
+      Map<String, String> result = data.get(i);
+      i++;
+      return result;
+    }
+    else{
+      throw new IllegalArgumentException("Tried to access next attribute list, but no new attribute list is available");
+    }
+  }
+
+  @Override
+  public boolean hasNewAttributeList() {
+    return i < data.size();
+  }
+}
