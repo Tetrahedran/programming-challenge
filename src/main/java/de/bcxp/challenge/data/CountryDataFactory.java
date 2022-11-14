88873d8971ea435c7d4edefdd8393ea51a66dfb7
Patrick <paddy38@web.de>
2022-11-14 16:21:20 +0100

    added regex check for numbers in csv that can be interpreted as integer but fail with standard parsing

--- src/main/java/de/bcxp/challenge/data/CountryDataFactory.java
+++ src/main/java/de/bcxp/challenge/data/CountryDataFactory.java
@@ -11,6 +11,12 @@ public class CountryDataFactory implements DataProvider<CountryData>{
   public static final String POPULATION_IDENTIFIER = "Population";
   public static final String AREA_IDENTIFIER = "Area";
 
+  /**
+   * Matches european styled semantic integer numbers that contain a decimal comma with following zeros and
+   * optional points as thousands separator
+   */
+  public static final String EUROPEAN_NUMBER_REGEX = "[[\\d]{1,3}[.]]*[\\d]{3}[,][0]+";
+
   @Override
   public List<CountryData> getDataObjectsFrom(AttributeListProvider provider) {
     Objects.requireNonNull(provider);
@@ -20,14 +26,20 @@ public class CountryDataFactory implements DataProvider<CountryData>{
     while(provider.hasNewAttributeList()){
       Map<String, String> attributes = provider.getNextAttributeList();
       String name = attributes.get(NAME_IDENTIFIER);
+      String populationString = attributes.get(POPULATION_IDENTIFIER);
       int population;
-      try {
-        Number number = NumberFormat.getInstance(Locale.GERMANY).parse(attributes.get(POPULATION_IDENTIFIER));
-        population = number.intValue();
+      if(populationString.matches(EUROPEAN_NUMBER_REGEX)){
+        try {
+          Number number = NumberFormat.getInstance(Locale.GERMANY).parse(attributes.get(POPULATION_IDENTIFIER));
+          population = number.intValue();
+        }
+        catch(ParseException pe){
+          throw new IllegalArgumentException("number " + attributes.get(POPULATION_IDENTIFIER) + " doesn't match" +
+            "the requested number format",pe);
+        }
       }
-      catch(ParseException pe){
-        throw new IllegalArgumentException("number " + attributes.get(POPULATION_IDENTIFIER) + " doesn't match" +
-          "the requested number format",pe);
+      else{
+        population = Integer.parseInt(populationString);
       }
       double area = Double.parseDouble(attributes.get(AREA_IDENTIFIER));
 
