ccbefa24ad7cf057e0af71bbe43df2ab0bfb1f00
Patrick <paddy38@web.de>
2022-11-14 11:26:55 +0100

    integrated countries task into app

--- src/main/java/de/bcxp/challenge/App.java
+++ src/main/java/de/bcxp/challenge/App.java
@@ -1,9 +1,11 @@
 package de.bcxp.challenge;
 
 import de.bcxp.challenge.data.CSVFileReader;
+import de.bcxp.challenge.data.CountryDataFactory;
 import de.bcxp.challenge.data.DataProvider;
 import de.bcxp.challenge.data.WeatherDataFactory;
 import de.bcxp.challenge.logic.BasicMetricEvaluator;
+import de.bcxp.challenge.model.CountryData;
 import de.bcxp.challenge.model.WeatherData;
 
 import java.io.InputStream;
@@ -23,19 +25,28 @@ public final class App {
     public static void main(String... args) {
 
         // Your preparation code …
-        List<WeatherData> data = new ArrayList<>();
-        String filePath = "de/bcxp/challenge/weather.csv";
-        InputStream stream = App.class.getClassLoader().getResourceAsStream(filePath);
-        try(CSVFileReader provider = new CSVFileReader(stream, filePath, ',')){
-            DataProvider<WeatherData> dataProvider = new WeatherDataFactory();
-            data = dataProvider.getDataObjectsFrom(provider);
+        List<WeatherData> wDataList = new ArrayList<>();
+        String weatherDataFilePath = "de/bcxp/challenge/weather.csv";
+        InputStream weatherStream = App.class.getClassLoader().getResourceAsStream(weatherDataFilePath);
+        try(CSVFileReader weatherStringProvider = new CSVFileReader(weatherStream, weatherDataFilePath, ',')){
+            DataProvider<WeatherData> weatherDataObjectProvider = new WeatherDataFactory();
+            wDataList = weatherDataObjectProvider.getDataObjectsFrom(weatherStringProvider);
+        }
+
+        List<CountryData> cDataList = new ArrayList<>();
+        String countryDataFilePath = "de/bcxp/challenge/countries.csv";
+        InputStream countryStream = App.class.getClassLoader().getResourceAsStream(countryDataFilePath);
+        try(CSVFileReader countryStringProvider = new CSVFileReader(countryStream, countryDataFilePath, ';')){
+            DataProvider<CountryData> countryDataObjectProvider = new CountryDataFactory();
+            cDataList = countryDataObjectProvider.getDataObjectsFrom(countryStringProvider);
         }
 
         String dayWithSmallestTempSpread = BasicMetricEvaluator.getIdentifierForMinimumMetric(
-          data, wData -> Integer.toString(wData.getDay()), WeatherData::getTemperatureSpread);     // Your day analysis function call …
+          wDataList, wData -> Integer.toString(wData.getDay()), WeatherData::getTemperatureSpread);     // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread: %s%n", dayWithSmallestTempSpread);
 
-        String countryWithHighestPopulationDensity = "Some country"; // Your population density analysis function call …
+        String countryWithHighestPopulationDensity = BasicMetricEvaluator.getIdentifierForMaximumMetric(
+          cDataList, CountryData::getName, CountryData::getPopulationDensity);
         System.out.printf("Country with highest population density: %s%n", countryWithHighestPopulationDensity);
     }
 }
