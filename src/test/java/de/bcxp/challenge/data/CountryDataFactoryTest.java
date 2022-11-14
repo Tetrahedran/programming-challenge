0c4773efec0ebc4c8fd489b19f9ea9325589144e
Patrick <paddy38@web.de>
2022-11-14 12:29:16 +0100

    added test for CountryDataFactory

---
+++ src/test/java/de/bcxp/challenge/data/CountryDataFactoryTest.java
@@ -0,0 +1,49 @@
+package de.bcxp.challenge.data;
+
+import de.bcxp.challenge.model.CountryData;
+import de.bcxp.challenge.model.WeatherData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CountryDataFactoryTest {
+  CountryDataFactory factory;
+
+  @BeforeEach
+  void setUp(){
+    factory = new CountryDataFactory();
+  }
+
+  @Test
+  void getDataObjectFromTest(){
+    String name = "Test";
+    int population = 0;
+    double area = 1;
+    CountryData expected = new CountryData(name, population, area);
+    List<Map<String, String>> mockData = new ArrayList<>();
+    Map<String, String> row1 = new HashMap<>();
+    row1.put(CountryDataFactory.NAME_IDENTIFIER, name);
+    row1.put(CountryDataFactory.POPULATION_IDENTIFIER, Integer.toString(population));
+    row1.put(CountryDataFactory.AREA_IDENTIFIER, Double.toString(area));
+    mockData.add(row1);
+    AttributeListProvider provider = new TestAttributeListProvider(mockData);
+    List<CountryData> dataList = factory.getDataObjectsFrom(provider);
+    assertEquals(mockData.size(), dataList.size());
+    CountryData cData = dataList.get(0);
+    assertEquals(expected, cData);
+  }
+
+  @Test
+  void getDataObjectsFromEmpty(){
+    List<Map<String, String>> mockData = new ArrayList<>();
+    AttributeListProvider provider = new TestAttributeListProvider(mockData);
+    List<CountryData> dataList = factory.getDataObjectsFrom(provider);
+    assertEquals(0, dataList.size());
+  }
+}
